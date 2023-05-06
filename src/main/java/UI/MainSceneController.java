d1d7eac14076b1b086a8fa9cfdeb8c8ff8c70214
Lizzyhara <liz.wellhausen@gmail.com>
2023-05-06 12:38:05 +0200

    Date Picker resticted & CheckBox in VBox

--- src/main/java/UI/MainSceneController.java
+++ src/main/java/UI/MainSceneController.java
@@ -1,15 +1,31 @@
 package UI;
 
+import java.io.IOException;
 import java.net.URL;
+import java.time.LocalDate;
 import java.util.ResourceBundle;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.DateCell;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
 public class MainSceneController implements Initializable {
 
+    @FXML
+    private VBox paneBox;
     @FXML
     private Label headerTitle;
     @FXML
@@ -22,10 +38,18 @@ public class MainSceneController implements Initializable {
     private ChoiceBox<String> trendLineBreaksChoice;
     @FXML
     private ChoiceBox<String> derivateChoice;
-
     @FXML
-    void btnOkClicked(ActionEvent event) {
-    }
+    private Pane sharePane;
+    @FXML
+    private DatePicker startPicker;
+    @FXML
+    private DatePicker endPicker;
+    @FXML
+    private VBox checkVBox;
+    @FXML
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
 
     @FXML
     private String[] categories = { "Option 1", "Option 2" };
@@ -37,7 +61,6 @@ public class MainSceneController implements Initializable {
     private String[] trends = { "Option 1", "Option 2" };
     @FXML
     private String[] derivate = { "Option 1", "Option 2" };
-
     @Override
     public void initialize(URL arg0, ResourceBundle arg1) {
         categorieChoice.getItems().addAll(categories);
@@ -46,5 +69,51 @@ public class MainSceneController implements Initializable {
         trendLineBreaksChoice.getItems().addAll(trends);
         derivateChoice.getItems().addAll(derivate);
     }
-
-}
+    @FXML
+    public void switchToMusicScene(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("MusicScene.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    public void addToCheckList(){
+        CheckBox cBox = new CheckBox("Hi");
+    
+        checkVBox.setPrefHeight((checkVBox.getChildren().size())*74.0);
+        checkVBox.getChildren().add(cBox);
+    }
+    @FXML
+    public void addToPaneBox(){
+        Pane examplePane = new Pane();
+        examplePane.getChildren().addAll(sharePane.getChildren());
+        examplePane.setPrefHeight(466);
+        examplePane.setPrefWidth(350);
+        examplePane.setBackground(sharePane.getBackground());
+        paneBox.setPrefHeight((paneBox.getChildren().size())*477.0);
+        paneBox.getChildren().add(examplePane);
+    }
+    
+    LocalDate minDateStart = LocalDate.of(2023, 4, 16);
+    LocalDate maxDateStart = LocalDate.now();
+    private void updateStartPicker(){
+    startPicker.setDayCellFactory(d ->
+            new DateCell() {
+               @Override public void updateItem(LocalDate item, boolean empty) {
+                      super.updateItem(item, empty);
+                   setDisable(item.isAfter(maxDateStart) || item.isBefore(minDateStart));
+                  }});
+                }
+    LocalDate minDateEnd = LocalDate.of(2023, 4, 16);
+    LocalDate maxDateEnd = LocalDate.now();
+    private void updateEndPicker(){
+    endPicker.setDayCellFactory(d ->
+            new DateCell() {
+               @Override public void updateItem(LocalDate item, boolean empty) {
+                      super.updateItem(item, empty);
+                   setDisable(item.isAfter(maxDateEnd) || item.isBefore(minDateEnd));
+                  }});
+                }  
+    
+    }
+               
--- src/main/java/UI/MusicSceneController.java
+++ src/main/java/UI/MusicSceneController.java
@@ -1,21 +1,39 @@
 package UI;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.scene.Node;
+import javafx.stage.Stage;
 
 public class MusicSceneController implements Initializable {
 
     @FXML
     private TextField headerTitle;
-
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+    
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'initialize'");
+       
     }
+    public void switchToMainScene(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("MainScene.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+       }
 
 }
