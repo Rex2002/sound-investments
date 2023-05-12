b5b82d69fb5f3e37bf61c05760b00d3354da4901
ArtInLines <linesinart@gmail.com>
2023-05-12 17:32:14 +0200

    Update indentation-size in App.java

--- src/main/java/UI/App.java
+++ src/main/java/UI/App.java
@@ -19,57 +19,57 @@ import state.EventQueues;
 // https://stackoverflow.com/a/60685975/13764271
 
 public class App extends Application {
-  // maintain a strong reference to the service
-  private CheckEQService service;
-
-  @Override
-  public void start(Stage primaryStage) {
-    try {
-      service = new CheckEQService();
-      service.setPeriod(Duration.millis(100));
-      service.setOnSucceeded(e -> {
-        List<Sonifiable> v = service.getValue();
-        if (!v.isEmpty()) {
-          for (Sonifiable s : v) {
-            System.out.println(s);
-          }
-        }
-      });
-
-      Parent root = FXMLLoader.load(getClass().getResource("MainScene.fxml"));
-      Scene scene = new Scene(root);
-      // scene.getStylesheets().add(getClass().getResource("Label.css").toExternalForm());
-      String css = this.getClass().getResource("choice.css").toExternalForm();
-      scene.getStylesheets().add(css);
-
-      primaryStage.setScene(scene);
-      primaryStage.show();
-
-      service.start();
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
-
-  }
-
-  public static void main(String[] args) {
-    launch(args);
-  }
-
-  private static class CheckEQService extends ScheduledService<List<Sonifiable>> {
-    static List<Sonifiable> l = new ArrayList<>(10);
+    // maintain a strong reference to the service
+    private CheckEQService service;
 
     @Override
-    protected Task<List<Sonifiable>> createTask() {
-      return new Task<>() {
-        @Override
-        protected List<Sonifiable> call() throws Exception {
-          l.clear();
-          while (!EventQueues.toUI.isEmpty())
-            l = EventQueues.toUI.poll();
-          return l;
+    public void start(Stage primaryStage) {
+        try {
+            service = new CheckEQService();
+            service.setPeriod(Duration.millis(100));
+            service.setOnSucceeded(e -> {
+                List<Sonifiable> v = service.getValue();
+                if (!v.isEmpty()) {
+                    for (Sonifiable s : v) {
+                        System.out.println(s);
+                    }
+                }
+            });
+
+            Parent root = FXMLLoader.load(getClass().getResource("MainScene.fxml"));
+            Scene scene = new Scene(root);
+            // scene.getStylesheets().add(getClass().getResource("Label.css").toExternalForm());
+            String css = this.getClass().getResource("choice.css").toExternalForm();
+            scene.getStylesheets().add(css);
+
+            primaryStage.setScene(scene);
+            primaryStage.show();
+
+            service.start();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-      };
+
     }
-  }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    private static class CheckEQService extends ScheduledService<List<Sonifiable>> {
+        static List<Sonifiable> l = new ArrayList<>(10);
+
+        @Override
+        protected Task<List<Sonifiable>> createTask() {
+            return new Task<>() {
+                @Override
+                protected List<Sonifiable> call() throws Exception {
+                    l.clear();
+                    while (!EventQueues.toUI.isEmpty())
+                        l = EventQueues.toUI.poll();
+                    return l;
+                }
+            };
+        }
+    }
 }
