178cde0e5de3dfecb389c266a52bb93f8c64fb87
ArtInLines <linesinart@gmail.com>
2023-05-27 12:34:57 +0200

    Updated EventQueues

--- src/main/java/app/communication/EventQueues.java
+++ src/main/java/app/communication/EventQueues.java
@@ -3,8 +3,12 @@ package app.communication;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 
+import audio.synth.playback.PlayControlEvent;
+
 public class EventQueues {
 	public static final BlockingQueue<Msg<MsgToUIType>> toUI = new ArrayBlockingQueue<>(10);
 
 	public static final BlockingQueue<Msg<MsgToSMType>> toSM = new ArrayBlockingQueue<>(10);
+
+	public static final BlockingQueue<PlayControlEvent> toPlayback = new ArrayBlockingQueue<>(10);
 }
