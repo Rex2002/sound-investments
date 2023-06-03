484a5fe69eef010bf6c55618c33a9277b53d4a79
malterichert <s211206@student.dhbw-mannheim.de>
2023-06-03 17:40:09 +0200

    fix(mixer): remove unused startPositions logic
    - all streams will start at index 0. If they only start later, they will hold 0s for the first values.
    - MixerException becomes unnecessary because no validation is done in the Mixer anymore
    - change input data to double[][] because there's no need for dynamic lists in the Sonifier

--- src/main/java/audio/mixer/Mixer.java
+++ src/main/java/audio/mixer/Mixer.java
@@ -1,48 +1,26 @@
 package audio.mixer;
 
-import java.util.ArrayList;
-
 public class Mixer {
     /**
      * Mixes the given audio streams together.
      *
-     * @param audioStreams   list of double-arrays
-     * @param startPositions list of start positions for each audio stream (int[0] should always be 0)
+     * @param audioStreams array of double-arrays to be mixed
      * @return mixed audio stream as double-array
-     * @throws MixerException if the length of the audio streams does not match
      */
-    public static double[] mixAudioStreams(ArrayList<double[]> audioStreams, int[] startPositions) throws MixerException {
-
-        // find the longest array's length
+    public static double[] mixAudioStreams(double[][] audioStreams) {
         int maxLength = 0;
-        for (int i = 0; i < audioStreams.size(); i++) {
-            maxLength = Math.max(maxLength, audioStreams.get(i).length + startPositions[i]);
+        for (double[] stream : audioStreams) {
+            maxLength = Math.max(maxLength, stream.length);
         }
-        double[] result = new double[maxLength];
 
-        // check if the start positions are valid
-        for (int i = 0; i < startPositions.length; i++) {
-            if (startPositions[i] > maxLength) {
-                throw new MixerException("Illegal start position (" + i + "), must be <= maxLength!");
-            }
-            if (startPositions[i] < 0) {
-                throw new MixerException("Illegal start position, must be >= 0");
+        double[] out = new double[maxLength];
+
+        for (double[] audioStream : audioStreams) {
+            for (int j = 0; j < audioStream.length; j++) {
+                out[j] += audioStream[j];
             }
         }
 
-        for (int i = 0; i < audioStreams.size(); i++) {
-            double[] audioStream = audioStreams.get(i);
-            int startPosition = startPositions[i];
-
-            if (audioStream.length + startPosition > result.length) {
-                throw new MixerException("Illegal array addition, length not matching! (Array " + i + " too long)");
-            }
-
-            for (int j = startPosition; j < audioStream.length + startPosition; j++) {
-                result[j] += audioStream[j];
-            }
-        }
-
-        return result;
+        return out;
     }
 }
