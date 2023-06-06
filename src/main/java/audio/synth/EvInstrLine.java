e91f93397693dea2be29e080f791a7ba156736a8
Rex2002 <frahm.benjamin@gmx.de>
2023-06-06 15:43:20 +0200

    feat: addresses comment in #102 by only allowing event instruments to be added at 16th notes

--- src/main/java/audio/synth/EvInstrLine.java
+++ src/main/java/audio/synth/EvInstrLine.java
@@ -22,7 +22,7 @@ public class EvInstrLine {
         int samplesPer16th = (int) (60 / ((double)TEMPO * 4) * SAMPLE_RATE * CHANNEL_NO);
         int lastCopy = - 2* samplesPer16th;
 
-        for(int i = 0; i < out.length; i++){
+        for(int i = 0; i < out.length; i+=samplesPer16th){
             if(data.values[Util.getRelPosition(i,out.length,data.values.length)] && lastCopy + 2 * samplesPer16th < i) {
                 lastCopy = i;
                 if(i + sample.length < out.length) System.arraycopy(sample, 0, out, i, sample.length);
