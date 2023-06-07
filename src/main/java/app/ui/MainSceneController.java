5eb7522dc91a7d611f28b114f733233bb404220f
ArtInLines <linesinart@gmail.com>
2023-06-07 07:10:07 +0200

    feat: MusicScene can also show errors now

--- src/main/java/app/ui/MainSceneController.java
+++ src/main/java/app/ui/MainSceneController.java
@@ -164,7 +164,7 @@ public class MainSceneController implements Initializable {
                     }
                     case ERROR -> {
                         startedSonification = false;
-                        displayError((String) msg.data, "Interner Fehler");
+                        CommonController.displayError(anchor, (String) msg.data, "Interner Fehler");
                         if (loadingAnimTimer != null) loadingAnimTimer.cancel();
                         if (loading != null) {
                             anchor.getChildren().remove(loading);
@@ -303,6 +303,7 @@ public class MainSceneController implements Initializable {
 
     public void switchToMusicScene(MusicData musicData) {
         try {
+            checkEQService.cancel();
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/MusicScene.fxml"));
             Parent root = loader.load();
             MusicSceneController controller = loader.getController();
@@ -316,43 +317,8 @@ public class MainSceneController implements Initializable {
             stage.show();
         } catch (IOException e) {
             e.printStackTrace();
-            displayError("Fehler beim Laden de nächsten UI-Szene", "Interner Fehler");
+            CommonController.displayError(anchor, "Fehler beim Laden de nächsten UI-Szene", "Interner Fehler");
         }
-    }
-
-    @FXML
-    private void displayError(String errorMessage, String errorTitle) {
-        // TODO: Make prettier & make text actually readable
-        System.out.println(errorMessage);
-        Pane errorPane = new Pane();
-        errorPane.setId("errorPane");
-        errorPane.setLayoutX(542);
-        errorPane.setLayoutY(200);
-        errorPane.setPrefHeight(500);
-        errorPane.toFront();
-        errorPane.setPrefWidth(500);
-        Label errorMes = new Label(errorMessage);
-        errorMes.setId("errorMessage");
-        errorMes.setLayoutY(50);
-        errorMes.setWrapText(true);
-
-        errorMes.setLayoutX(20);
-        Label errorTit = new Label(errorTitle);
-        errorTit.setId("errorTitle");
-        errorTit.setLayoutY(20);
-        errorTit.setLayoutX(20);
-
-        errorTit.setWrapText(true);
-        Button close = new Button("Schließen");
-        close.setOnMouseClicked(event ->{
-                anchor.getChildren().remove(errorPane);
-        });
-        close.setLayoutX(330);
-        close.setLayoutY(20);
-        close.setId("closeBtn");
-        errorPane.getChildren().addAll(errorMes, close, errorTit);
-        anchor.getChildren().add(errorPane);
-
     }
 
     void clearCheckList() {
@@ -374,7 +340,7 @@ public class MainSceneController implements Initializable {
                 if (paneBoxSonifiables.getChildren().size() < Mapping.MAX_SONIFIABLES_AMOUNT) {
                     stockPane[0]=  addToPaneBox((Sonifiable) cBox.getUserData());
                 } else {
-                    displayError(
+                    CommonController.displayError(anchor,
                             "Zu viele Börsenkurse gewählt. Es dürfen höchstens "
                                     + Integer.toString(Mapping.MAX_SONIFIABLES_AMOUNT) + " Börsenkurse gewählt werden.",
                             "Zu viele Börsenkurse");
@@ -473,7 +439,7 @@ public class MainSceneController implements Initializable {
                     if (oldValue != null) mapping.rmEvInstr(sonifiable.getId(), (PointData) eparam);
                     if (newValue != null) mapping.addEvInstr(EvInstrEnum.fromString(newValue), sonifiable, (PointData) eparam);
                 } catch (AppError e) {
-                    displayError(e.getMessage(), "Interner Fehler");
+                    CommonController.displayError(anchor, e.getMessage(), "Interner Fehler");
                 }
             });
             children.add(evInstCB);
@@ -536,7 +502,7 @@ public class MainSceneController implements Initializable {
                     enableBtnIfValid();
                     currentlyUpdatingCB = false;
                 } catch (AppError e) {
-                    displayError(e.getMessage(), "Interner Fehler");
+                    CommonController.displayError(anchor, e.getMessage(), "Interner Fehler");
                 }
             });
             paramCB.getSelectionModel().selectedItemProperty().addListener((observable, oldStr, newStr) -> {
@@ -562,7 +528,7 @@ public class MainSceneController implements Initializable {
                     currentlyUpdatingCB = false;
                 } catch (AppError e) {
                     paramCB.getSelectionModel().select(oldStr);
-                    displayError(e.getMessage(), "Interner Fehler");
+                    CommonController.displayError(anchor, e.getMessage(), "Interner Fehler");
                 }
             });
 
