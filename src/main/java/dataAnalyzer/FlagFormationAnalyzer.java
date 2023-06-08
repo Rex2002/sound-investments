94e6c3d39e2584dbe5a6fa887c2712e4b464269e
JakobPK <s211200@student.dhbw-mannheim.de>
2023-06-08 23:40:42 +0200

    gaussian blurr, trendbreaks, adapted flag

--- src/main/java/dataAnalyzer/FlagFormationAnalyzer.java
+++ src/main/java/dataAnalyzer/FlagFormationAnalyzer.java
@@ -2,52 +2,47 @@ package dataAnalyzer;
 
 import java.util.List;
 import dataRepo.Price;
+import dataAnalyzer.Blurr;
 
 public class FlagFormationAnalyzer {
 	public static boolean[] analyze(List<Price> priceList) {
 		boolean[] out = new boolean[priceList.size()];
 		out[0] = false;
+		double[] blurredValues = new double[priceList.size()];
+		blurredValues = gaussianBlur(priceList);
 
-		for (int i = 1; i < priceList.size() - 1; i++) {
-			Price currentDay = priceList.get(i);
-			int formationLength = 0;
+		
+		int formationLength = 0;
+    	boolean isRising = false;
+    	boolean isFalling = false;
+    	boolean findFlag = false;
+    	boolean isFlag = false;
 
-			// Überprüfe, ob die aktuellen Tageswerte die Bedingungen für eine Flaggen-Formation erfüllen
-			if (currentDay.getClose() > priceList.get(i - 1).getClose()
-				&& currentDay.getClose() > priceList.get(i + 1).getClose()
-				&& currentDay.getHigh() > priceList.get(i - 1).getHigh()
-				&& currentDay.getHigh() > priceList.get(i + 1).getHigh()
-				&& currentDay.getLow() < priceList.get(i - 1).getLow()
-				&& currentDay.getLow() < priceList.get(i + 1).getLow()) {
+		for (int i = 1; i < priceList.size(); i++) {
+			double values = blurredValues[i];
+			if (values > blurredValues[i - 1] && !isFalling) {
+				findFlag = true;
+				isRising = true;
+				formationLength++;
+			  } else if (values < blurredValues[i - 1] && findFlag) {
+				isRising = false;
+				isFalling = true;
+				formationLength++;
+				isFlag = true;
+			  }
 
-				// Überprüfe, wie lange die Formation anhält (von 3 bis 60 Tagen)
-				int j = i - 1;
-				while (j >= 0 && currentDay.getClose() > priceList.get(j).getClose()
-					&& currentDay.getHigh() > priceList.get(j).getHigh()
-					&& currentDay.getLow() < priceList.get(j).getLow()
-					&& i - j <= 60) {
-						formationLength++;
-						j--;
-				}
-				j = i + 1;
-				while (j < priceList.size() && currentDay.getClose() > priceList.get(j).getClose()
-					&& currentDay.getHigh() > priceList.get(j).getHigh()
-					&& currentDay.getLow() < priceList.get(j).getLow()
-					&& j - i <= 60) {
-						formationLength++;
-						j++;
-				}
-			}
-
-			if (formationLength >= 2 && formationLength <= 60) {
+			  if (values > blurredValues[i - 1] && isFlag && formationLength >= 2 && formationLength <= 60) {
 				for (int k = 0; k < formationLength; k++) {
-					out[i-k] = true;
+				  out[i - k] = true;
 				}
-			} else {
+				formationLength = 0;
+				isFlag = false;
+			  } else {
 				out[i] = false;
 			}
 		}
 
 		return out;
 	}
+
 }
