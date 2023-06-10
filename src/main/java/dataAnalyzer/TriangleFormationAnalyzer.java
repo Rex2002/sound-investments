020467f0a6b75b87c0f7e7fe8e59d1b957dab782
JakobPK <s211200@student.dhbw-mannheim.de>
2023-06-10 13:49:19 +0200

    fixed issue 170 - triangle formation

--- src/main/java/dataAnalyzer/TriangleFormationAnalyzer.java
+++ src/main/java/dataAnalyzer/TriangleFormationAnalyzer.java
@@ -10,38 +10,32 @@ public class TriangleFormationAnalyzer {
         boolean[] triangleformations = new boolean[priceList.size()];
         double[] blurredValues = Blur.averageBlur(priceList);
 
-        for (int i = 0; i < triangleformations.length; i++) {
-            double lastPrice = blurredValues[i];
+        double compDif = Math.abs(blurredValues[0] - blurredValues[1]);
+    double maxDifference = 1.0;
+    int formationLen = 0;
+    boolean end = false;
 
-            // Check for decreasing formation
-            int formationLen = 0;
-            for (int j = i + 1; j < triangleformations.length; j++, formationLen++) {
-                double currentPrice = blurredValues[j];
-                if (currentPrice > lastPrice)
-                    break;
-                lastPrice = currentPrice;
-            }
-
-            // Check for increasing formation
-            if (formationLen == 0) {
-                for (int j = i + 1; j < triangleformations.length; j++, formationLen++) {
-                    double currentPrice = blurredValues[j];
-                    if (currentPrice < lastPrice)
-                        {break;}
-                    lastPrice = currentPrice;
-                }
-            }
-
-            if (formationLen >= MIN_FORMATION_LEN) {
-                for (int k = i; k < i + formationLen; k++) {
-                    triangleformations[k] = true;
-                }
-                i += formationLen;
-            } else {
-                triangleformations[i] = false;
-            }
+    for (int i = 2; i < blurredValues.length; i++) {
+      double currentDifference = Math.abs(blurredValues[i] - blurredValues[i - 1]);
+      if (currentDifference > maxDifference && currentDifference < compDif) {
+        formationLen++;
+      } else if (currentDifference <= maxDifference && currentDifference < compDif) {
+        end = true;
+      }
+      if (end&&MIN_FORMATION_LEN<=formationLen) {
+        for (int j = 0; j <= formationLen; j++) {
+          triangleformations[i - j] = true;
+          triangleformations[i - j - 1] = true;
+          triangleformations[i - j - 2] = true;
         }
+        end = false;
+        formationLen = 0;
+      } else {
+        triangleformations[i] = false;
+      }
+      compDif = currentDifference;
+    }
 
-        return triangleformations;
+    return triangleformations;
     }
 }
