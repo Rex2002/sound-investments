8713bb6dc75227417eb7f5caf88d205d1e3345b7
malterichert <s211206@student.dhbw-mannheim.de>
2023-06-10 23:06:13 +0200

    chore(backing): add javaDoc for Backing

--- src/main/java/audio/mixer/Backing.java
+++ src/main/java/audio/mixer/Backing.java
@@ -14,25 +14,14 @@ public class Backing {
     private double[][] fills;
     private final Random random = new Random();
 
-    public double[] getBacking( int bars ) {
-        int seconds = (int) Math.ceil( (bars * 4) / (Constants.TEMPO / 60f) );
-        double[] out = new double[seconds * Constants.SAMPLE_RATE * Constants.CHANNEL_NO];
-
-        int bar = 0, offset = 0;
-        while (bar < bars) {
-            double[] sample = chooseSample(bar);
-
-            System.arraycopy(sample, 0, out, offset, sample.length);
-
-            offset += sample.length;
-            bar += SAMPLE_BARS;
-        }
-
-        return out;
-    }
-
+    /**
+     * Selects a random backing sample from resources/audio/backings and returns the sonification's tempo as stated in
+     * the backing's filename. The selected sample and the fills associated with it are safed in the object's field vars
+     * for use in getBacking().
+     * @return selected tempo in bpm (quarter-note)
+     * @throws AppError when there are no backing tracks matching the name schema in resources/audio/backings
+     */
     public int setSamplesAndGetTempo() throws AppError {
-        //TODO: I think this method of getting file names doesn't work in a JAR. Needs to be changed for release
         File directory = new File("./src/main/resources/audio/backings");
         List<String> backings = List.of(Objects.requireNonNull(directory.list()));
 
@@ -60,12 +49,38 @@ public class Backing {
         return Integer.parseInt(metaData[0]);
     }
 
+    /**
+     * Gets the previously selected backing track and loops it for the desired length,
+     * alternating with a corresponding fill.
+     * @param bars length of sonification in multiple of four 4/4-bars of given tempo
+     * @return audio line of randomly selected backing groove and fill
+     */
+    public double[] getBacking( int bars ) {
+        int seconds = (int) Math.ceil( (bars * 4) / (Constants.TEMPO / 60f) );
+        double[] out = new double[seconds * Constants.SAMPLE_RATE * Constants.CHANNEL_NO];
+
+        int bar = 0, offset = 0;
+        while (bar < bars) {
+            double[] sample = chooseSample(bar);
+
+            System.arraycopy(sample, 0, out, offset, sample.length);
+
+            offset += sample.length;
+            bar += SAMPLE_BARS;
+        }
+
+        return out;
+    }
+
 
     /**
-     * this currently assumes that all backing samples are of length 4
+     * All backing samples have to be of equal length SAMPLE_BARS in bars of the tempo in the filename, including the fills.
+     * There can only be one groove of a given name, but it can have multiple fills associated with it.
+     * They have the same name and simply increment the index in their filename.
+     * @param currentBar the starting bar of the sample to be chosen
      */
-    private double[] chooseSample(int bar) {
-        if (bar % 8 == 4) {
+    private double[] chooseSample(int currentBar) {
+        if (currentBar % 8 == 4) {
             return fills[random.nextInt(fills.length)];
         } else {
             return groove;
