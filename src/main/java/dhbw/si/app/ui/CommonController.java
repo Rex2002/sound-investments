b363f092aca1d505ee2a33ca53a7f56a7a420758
ArtInLines <64645307+ArtInLines@users.noreply.github.com>
2023-06-12 08:45:13 +0200

    feat: smaller UI (#245)

--- src/main/java/dhbw/si/app/ui/CommonController.java
+++ src/main/java/dhbw/si/app/ui/CommonController.java
@@ -3,28 +3,30 @@ package dhbw.si.app.ui;
 import javafx.scene.layout.Pane;
 import dhbw.si.util.DateUtil;
 import javafx.scene.control.Label;
+import javafx.scene.Cursor;
 import javafx.scene.control.Button;
 
 public class CommonController {
 	public static void displayError(Pane parent, String errorMessage, String errorTitle) {
+        double width = 400;
         Pane errorPane = new Pane();
         errorPane.setId("errorPane");
-        errorPane.setLayoutX(593);
-        errorPane.setLayoutY(262.5);
+        errorPane.setLayoutX((parent.getWidth() - width)/2);
+        errorPane.setLayoutY(240);
         errorPane.setPrefHeight(300);
         errorPane.toFront();
-        errorPane.setPrefWidth(400);
+        errorPane.setPrefWidth(width);
         Label errorMes = new Label(errorMessage);
         errorMes.setId("errorMessage");
         errorMes.setLayoutY(40);
         errorMes.setWrapText(true);
-        
+
         errorMes.setLayoutX(20);
         Label errorTit = new Label(errorTitle);
         errorTit.setId("errorTitle");
         errorTit.setLayoutY(20);
         errorTit.setLayoutX(30);
-        
+
         errorTit.setWrapText(true);
         Button close = new Button("Schließen");
         close.setOnMouseClicked(event ->{
@@ -33,6 +35,7 @@ public class CommonController {
         close.setLayoutX(257.5);
         close.setLayoutY(20);
         close.setId("closeBtn");
+        close.setCursor(Cursor.HAND);
         errorPane.getChildren().addAll(errorMes, close, errorTit);
         parent.getChildren().add(errorPane);
     }
